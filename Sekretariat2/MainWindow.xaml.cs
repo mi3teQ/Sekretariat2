e510270c5f271b804b07b3aec8e55c9cde667b91
stani <nowakstas6@gmail.com>
2021-12-04 19:24:17 +0100

    komit

--- Sekretariat2/MainWindow.xaml.cs
+++ Sekretariat2/MainWindow.xaml.cs
@@ -18,11 +18,68 @@ namespace Sekretariat2
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+    
     public partial class MainWindow : Window
     {
+
+        public static MainWindow AppWindow;
         public MainWindow()
         {
             InitializeComponent();
+            AppWindow = this;
+
+        }
+
+        public void myFunc(string imie)
+        {
+            ListView_Uczniowie.Items.Add(imie);
+        }
+
+        private void Btn_Dodaj_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedIndex == 0)
+            {
+                var Uczniowie = new Uczniowie();
+                Uczniowie.Show();
+                this.Close();
+            }
+            if (TabControl.SelectedIndex == 1)
+            {
+                var Nauczyciele = new Nauczyciele();
+                Nauczyciele.Show();
+                this.Close();
+            }
+            if (TabControl.SelectedIndex == 2)
+            {
+                var Pracownicy = new Pracownicy();
+                Pracownicy.Show();
+                this.Close();
+            }
+            
+        }
+
+        private void Btn_Zmien_Click(object sender, RoutedEventArgs e)
+        {
+            if (TabControl.SelectedIndex == 0)
+            {
+                var Uczniowie = new Uczniowie();
+                Uczniowie.Show();
+                this.Close();
+            }
+            if (TabControl.SelectedIndex == 1)
+            {
+                var Nauczyciele = new Nauczyciele();
+                Nauczyciele.Show();
+                this.Close();
+            }
+            if (TabControl.SelectedIndex == 2)
+            {
+                var Pracownicy = new Pracownicy();
+                Pracownicy.Show();
+                this.Close();
+            }
         }
     }
 }
---
+++ Sekretariat2/Uczniowie.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sekretariat2
+{
+    /// <summary>
+    /// Interaction logic for Uczniowie.xaml
+    /// </summary>
+    public partial class Uczniowie : Window
+    {
+        public Uczniowie()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Wroc_Click(object sender, RoutedEventArgs e)
+        {
+            var MainWindow = new MainWindow();
+            MainWindow.Show();
+            this.Close();
+        }
+
+        private void Btn_Dodaj_Click(object sender, RoutedEventArgs e)
+        {
+            
+            //string[] row = { Txtbox_Imie.Text, Txtbox_Nazwisko.Text };
+
+            Sekretariat2.MainWindow.AppWindow.myFunc("Zdzichu");
+
+            //MainWindow.ListView_Uczniowie.Items.Add(row);
+        }
+    }
+}
